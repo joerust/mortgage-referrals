beeae47c69df1d03c462173e5e532adb4e6ccea9
joerust <joseph.rust@53.com>
2016-09-23 14:30:58 -0400

    Add files via upload
    Logging

--- referral_chaincode.go
+++ referral_chaincode.go
@@ -360,8 +360,11 @@ func (t *ReferralChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byt
 	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
+		return []byte(jsonResp), errors.New(jsonResp)
 	}
 
+	if valAsbytes == nil {
+		return []byte("Did not find entry for key: " + key), nil
+	}
 	return valAsbytes, nil
 }
