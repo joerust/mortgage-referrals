287efe62f34745dcc10c6006a5bde0b713b63c94
joerust <joseph.rust@53.com>
2016-09-23 15:32:09 -0400

    Add files via upload
    Commit

--- referral_chaincode.go
+++ referral_chaincode.go
@@ -308,17 +308,21 @@ func (t *ReferralChaincode) searchByStatus(status string, stub *shim.ChaincodeSt
 func (t *ReferralChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
-
+	
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
 	key = args[0]
 	valAsbytes, err := stub.GetState(key)
+	
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
+		return []byte(jsonResp), err
 	}
-
+	
+	if valAsbytes == nil {
+		return []byte("Did not find entry for key: " + key), nil
+	}
 	return valAsbytes, nil
 }
